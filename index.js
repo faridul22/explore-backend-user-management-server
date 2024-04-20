1cba074f3e99e3e9ff54f3c66cc69c1943dd5a25
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-04-20 19:11:26 +0600

    user added and user api created

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const users = [
+    { id: 1, name: "Farid", category: "job" },
+    { id: 2, name: "Santa", category: "housewife" },
+    { id: 3, name: "Rafat", category: "student" },
+    { id: 4, name: "Fatiha", category: "student" },
+    { id: 5, name: "Enjamam", category: "normal" },
+]
 
 app.get('/', (req, res) => {
     res.send("User management server is running")
 });
+
+app.get("/users", (req, res) => {
+    res.send(users)
+})
 
 app.listen(port, () => {
     console.log(`User Management server in running on port:${port}`)
