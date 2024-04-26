9ca24788e6bc7fc7fc9841c11f72cfc57dfc19f5
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-04-26 19:30:27 +0600

    user post api created

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+// middleware
+app.use(cors());
+app.use(express.json())
 
 const users = [
     { id: 1, name: "Farid", category: "job" },
@@ -21,6 +23,14 @@ app.get("/users", (req, res) => {
     res.send(users)
 })
 
+app.post('/users', (req, res) => {
+    console.log("hitting post api")
+    console.log(req.body)
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    res.send(newUser)
+})
 app.listen(port, () => {
     console.log(`User Management server in running on port:${port}`)
 })
