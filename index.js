845d008fe4d4096afde627cc74815dae2f2efd07
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-08-18 16:42:53 +0600

    basic user created

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const user = [
+    { name: "Farid", age: 25 },
+    { name: "jalal", age: 52 },
+    { name: "kamal", age: 45 },
+    { name: "salam", age: 53 },
+]
+
 app.get('/', (req, res) => {
     res.send('user management server is Running')
+})
+
+app.get('/user', (req, res) => {
+    res.send(user)
 })
 
 app.listen(port, () => {
