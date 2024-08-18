135d995a7218d3595993d31f0aedaab487f9ff06
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-08-18 16:37:05 +0600

    basic server start again for practice

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-// middleware
-app.use(cors());
-app.use(express.json())
-
-const users = [
-    { id: 1, name: "Farid", category: "job" },
-    { id: 2, name: "Santa", category: "housewife" },
-    { id: 3, name: "Rafat", category: "student" },
-    { id: 4, name: "Fatiha", category: "student" },
-    { id: 5, name: "Enjamam", category: "normal" },
-]
-
 app.get('/', (req, res) => {
-    res.send("User management server is running")
-});
-
-app.get("/users", (req, res) => {
-    res.send(users)
+    res.send('user management server is Running')
 })
 
-app.post('/users', (req, res) => {
-    console.log("hitting post api")
-    console.log(req.body)
-    const newUser = req.body;
-    newUser.id = users.length + 1;
-    users.push(newUser)
-    res.send(newUser)
-})
-// add new comment for checked file
 app.listen(port, () => {
-    console.log(`User Management server in running on port:${port}`)
+    console.log(`user management server is running on port ${port}`)
 })
