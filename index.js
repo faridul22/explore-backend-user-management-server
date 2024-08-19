c7a8b9fe20bc0415143a20c0796f010eb0ce5db6
faridul22 <96929890+faridul22@users.noreply.github.com>
2024-08-19 10:59:28 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
 const port = process.env.PORT || 5000;
 
-const user = [
-    { name: "Farid", age: 25 },
-    { name: "jalal", age: 52 },
-    { name: "kamal", age: 45 },
-    { name: "salam", age: 53 },
+const users = [
+    { id: 1, email: "Farid", password: 25 },
+    { id: 2, email: "jalal", password: 52 },
+    { id: 3, email: "kamal", password: 45 },
+    { id: 4, email: "salam", password: 53 },
 ]
 
 app.get('/', (req, res) => {
     res.send('user management server is Running')
 })
 
-app.get('/user', (req, res) => {
-    res.send(user)
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser)
 })
 
 app.listen(port, () => {
